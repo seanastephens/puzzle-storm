af1d5882115ac91bada27154de2505dbe9d94867
Otasc12 <cyguy99353@gmail.com>
2015-12-18 22:53:44 -0800

    New moving chessboard.

--- puzzle.py
+++ puzzle.py
@@ -4,26 +4,39 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((512, 512))
+screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('PUZZLES BAM!!!')
 
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((250, 250, 250))
+BLACK = ( 0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = ( 0, 0, 255)
 
-def drawBoard():
-    pass # replace this with drawing code
 
+def drawBoard(t):
+	screen.fill(WHITE)
+	width = 50	
+	for x in range(0, 500, 50):
+		for y in range(0, 500, 50):
+			if (x + y) % 100 == 0:
+				color = BLUE
+			else:
+				color = BLACK
+			pygame.draw.rect(screen, color, [x + (t % 50), y, width, width])
+
+	pygame.display.flip()
+	
 clock = pygame.time.Clock()
 
 running = True
+ticker = 0
 while running:
     clock.tick(60)
     for event in pygame.event.get():
         # print event
         if event.type == QUIT:
             running = False
-    drawBoard()
+    drawBoard(ticker)
+    ticker += 1
 
 
 pygame.quit()
