8a250445d1bead98f4e53d0f630c717407ba3ab0
Sean Stephens <seanastephens@email.arizona.edu>
2015-12-18 20:40:06 -0800

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((512, 512))
+pygame.display.set_caption('What will this be...')
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((250, 250, 250))
+
+# Handle Input Events
+while True:
+    for event in pygame.event.get():
+        print event
+
+pygame.quit()
