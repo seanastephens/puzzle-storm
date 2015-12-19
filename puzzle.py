17087b2f34232e4fab6a385cf150c81a0622cb40
Otasc12 <cyguy99353@gmail.com>
2015-12-18 20:48:03 -0800

    Update caption

--- puzzle.py
+++ puzzle.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((512, 512))
-pygame.display.set_caption('What will this be...')
+pygame.display.set_caption('PUZZLES BAM!!!')
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
